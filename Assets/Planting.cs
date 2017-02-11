b0c8c2f005075d9ca5309d515bea22f154fb3136
Nisssha <nisha@vp.pl>
2017-02-11 19:35:17 +0100

    FlowerQuest baby version

---
+++ Assets/Planting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Planting : MonoBehaviour {
+
+	public GameObject narcis;
+
+void Update(){
+	//	if (Input.GetMouseButtonDown(1)){
+	//
+	//		Click();
+	//	}
+}
+
+	public void Click(){
+
+			Debug.Log ("Pot clicked");
+			Vector3 flowerPosition = new Vector3(this.transform.position.x+6, this.transform.position.y +35, this.transform.position.z);
+
+			GameObject flower = Instantiate(narcis, flowerPosition, Quaternion.identity);
+			flower.transform.SetParent(GameObject.FindObjectOfType<Button>().transform, false);
+
+		
+	}
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Game  {
+
+public static Game current;
+public Flower daisy;
+public Flower redRose;
+public Flower dandelion;
+//public string yas;
+
+public Game () {
+	daisy = new Flower();
+	redRose = new Flower();
+	dandelion = new Flower();
+	//Debug.Log("Constructor is calledd?");
+}
+
+}
